f376f859a5be7bb22497f6e30fc85094c213b7e8
wannabeemetal <siddharth4u2003@yahoo.com>
2013-11-22 16:35:49 -0800

    Commiting changes

--- New Unity Project/Assets/drawLine.cs
+++ New Unity Project/Assets/drawLine.cs
@@ -12,11 +12,13 @@ public class drawLine : MonoBehaviour {
 	private float counter;
 	RaycastHit hit;
 	float reach = 100;
+	float xe = 1;
+	float theta3;
 	
 	void Start () 
 	{
 		lineRenderer = GetComponent<LineRenderer>();
-		lineRenderer.SetPosition(0, origin.position);
+		lineRenderer.SetPosition(0, destination.position);
 		lineRenderer.SetWidth(0.45f, 0.45f);
 		dist = Vector3.Distance(origin.position,destination.position);
 		points = new Transform[20];
@@ -27,21 +29,45 @@ public class drawLine : MonoBehaviour {
 	
 	void Update () 
 	{
-		float theta3 = Mathf.Rad2Deg*Mathf.Atan((Mathf.Abs(points[0].position.x) - Mathf.Abs(points[1].position.x))/(Mathf.Abs(points[0].position.y) - Mathf.Abs(points[1].position.y)));
-		//print("theta1 "+points[1].eulerAngles.y);
-		float theta2 = (90 - (points[1].eulerAngles.y + theta3));
+		lineRenderer.SetPosition(0, points[1].position);
+		float slope = (points[0].position.z - points[1].position.z)/(points[0].position.x - points[1].position.x); // find slope
+		
+		theta3 = 90-(Mathf.Rad2Deg*Mathf.Atan(slope) - points[1].eulerAngles.y); //find angle between line and reflection
 		//print ("theta3 "+theta3);
-		//print ("theta2 "+theta2);
-		float xe = points[0].position.x + 2*(points[1].position.x - points[0].position.x) ;
+		
+		print ("t3 "+theta3 + " y "+points[1].eulerAngles.y);
 
-		//print (theta2);
+		theta3 = theta3 % 360; // so that angle always lies between 0-360
+
+		if (theta3 < 0)
+		{
+	 	   theta3 += 360;
+		}
+		theta3 = theta3*3.14f/180f;  //converts angle to radians
+		float roty = points[1].eulerAngles.y;
+		roty = roty*3.14f/180f;      //roa
+		slope = Mathf.Tan(theta3);
+		print ("Slope "+slope);
+		float theta2 = (90 - (points[1].eulerAngles.y + theta3));
+		print ("t2 "+theta2);
+		//float xe = points[0].position.x + 2*(points[1].position.x - points[0].position.x) ;
+		if(xe == 300)
+			xe = points[1].position.x;
+		//print (slope);
 		theta2 = -1*theta2*3.14f/180f;
-		float y = (Mathf.Tan(theta2))*xe+ (points[0].position.y - points[1].position.y);
-		print (Mathf.Tan(theta2));
+		xe = 100;
+		float z = -1*slope*xe + points[0].position.z;
+		if(points[0].position.x>points[1].position.x || slope<0)
+		{
+			xe = -1*xe;
+		}
+		//float y = (Mathf.Tan(theta2))*xe+ (points[0].position.y - points[1].position.y);
+		//print (Mathf.Tan(theta2));
 
 		
-		Vector3 reflection = new Vector3(xe,y,(points[0].position.y - points[1].position.y));
-		Vector3 reflectionx = new Vector3(xe,y,(points[0].position.y - points[1].position.y));
+		Vector3 reflection = new Vector3(xe,0,z);
+				lineRenderer.SetPosition(1, (points[1].position - points[0].position)+reflection);
+//		Vector3 reflectionx = new Vector3(xe,y,(points[0].position.y - points[1].position.y));
 		Debug.DrawLine(origin.position,(calculateReflection(points[0],points[1])));
 		Debug.DrawLine(destination.position,reflection,Color.red);
 		Debug.DrawRay(origin.position,destination.position - origin.position,Color.white);
---
+++ New Unity Project/Assets/drawLineAgain.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drawLineAgain : MonoBehaviour {
+
+	public Transform origin;
+	public Transform destination;
+	private Transform[] points;
+	private LineRenderer lineRenderer;
+	private float dist;
+	public float lineSpeed;
+	private float counter;
+	float reach = 100;
+	float xe = 1;
+	float theta3;
+
+	
+	void Start () 
+	{
+		lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.SetPosition(0, destination.position);
+		lineRenderer.SetWidth(0.45f, 0.45f);
+		dist = Vector3.Distance(origin.position,destination.position);
+		points = new Transform[20];
+		points[0] = origin;
+		points[1] = destination;
+		//calculatePoints();
+	}
+	
+	void Update () 
+	{
+		drawLaser(points[0].position,3);
+	}
+
+	Vector3 calculateReflection(Transform a, Transform b)
+	{
+		//float theta = transform.eulerAngles.y + 90+temp;
+		//dir=new Vector3((-1)*reach*Mathf.Cos(theta),0,reach*Mathf.Sin(theta));
+		Vector3 temp = Vector3.zero;
+		temp.x = a.position.x + 2*(b.position.x - a.position.x);
+		Debug.DrawLine(a.position,temp);
+		return temp;
+	}
+	void drawLaser(Vector3 startPoint,int n)  
+	{
+    Vector3 rayDir = transform.TransformDirection (Vector3.forward);
+ 		RaycastHit hit;
+    for(int i = 0; i < n; i++)
+    {     
+        if (Physics.Raycast (startPoint, rayDir, out hit, 1000f)) 
+        {
+           Debug.DrawLine (startPoint, hit.point);
+           rayDir = Vector3.Reflect( (hit.point - startPoint).normalized, hit.normal  ) ;
+               startPoint = hit.point;
+        }
+			else
+			{
+				Debug.DrawLine (startPoint, rayDir*1000f);
+			}
+    }
+}
+}
